8f2a7ce74a455340ba534ab3c62eed92402f17a5
August Soderberg <asoderberg@brandeis.edu>
2020-10-21 22:16:38 +0000

    Emergency Save

--- line_follower/src/follower_opencv.py
+++ line_follower/src/follower_opencv.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import rospy, cv2, cv_bridge, numpy
+from constants import *
 from sensor_msgs.msg import Image
 
 class CVImgSubPub:
@@ -14,8 +15,25 @@ class CVImgSubPub:
         lower_yellow = numpy.array([18, 0, 0])
         upper_yellow = numpy.array([50, 255, 255])
         image = cv2.inRange(image, lower_yellow, upper_yellow)
+        top = find_middle_white(image, TOP_HORIZONTAL)
+        bottom = find_middle_white(image, IMAGE_HEIGHT - 1)
         self.image_pub.publish(self.bridge.cv2_to_imgmsg(image))
-        print("published")
+        print(top + "   " + bottom)
+    
+    def find_middle_white(self, image, horiz):
+        found_white = False
+        first_white = -1
+        last_white = -1
+        for i in range(0, IMAGE_WIDTH):
+            if image[horiz, i] == WHITE:
+                if not found_white:
+                    first_white = i
+                    found_white = True
+                else:
+                    last_white = i 
+        return (last_white + first_white) // 2
+
+    
         
 
 rospy.init_node('cv_img_sub_pub')
