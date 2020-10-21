2a6fb06d4f534a656747b1eb8bf564dba15679bc
August Soderberg <asoderberg@brandeis.edu>
2020-10-21 22:48:31 +0000

    Add slope and center publishers

--- line_follower/src/follower_opencv.py
+++ line_follower/src/follower_opencv.py
@@ -2,6 +2,21 @@
 import rospy, cv2, cv_bridge, numpy
 from constants import *
 from sensor_msgs.msg import Image
+from std_msgs.msg import Int32, Float64
+
+def find_middle_white(image, horiz):
+        horiz = int(horiz)
+        found_white = False
+        first_white = -1
+        last_white = -1
+        for i in range(0, IMAGE_WIDTH):
+            if image[horiz, i] == WHITE:
+                if not found_white:
+                    first_white = i
+                    found_white = True
+                else:
+                    last_white = i 
+        return (last_white + first_white) // 2
 
 class CVImgSubPub:
     def __init__(self):
@@ -17,26 +32,17 @@ class CVImgSubPub:
         image = cv2.inRange(image, lower_yellow, upper_yellow)
         top = find_middle_white(image, TOP_HORIZONTAL)
         bottom = find_middle_white(image, IMAGE_HEIGHT - 1)
+        center_pub.publish((top + bottom) // 2)
+        slope_pub.publish((IMAGE_HEIGHT - TOP_HORIZONTAL) / (top - bottom))
         self.image_pub.publish(self.bridge.cv2_to_imgmsg(image))
-        print(top + "   " + bottom)
-    
-    def find_middle_white(self, image, horiz):
-        found_white = False
-        first_white = -1
-        last_white = -1
-        for i in range(0, IMAGE_WIDTH):
-            if image[horiz, i] == WHITE:
-                if not found_white:
-                    first_white = i
-                    found_white = True
-                else:
-                    last_white = i 
-        return (last_white + first_white) // 2
+        print((top + bottom) // 2, (IMAGE_HEIGHT - TOP_HORIZONTAL) / (top - bottom))
 
     
         
 
 rospy.init_node('cv_img_sub_pub')
 cvImgSubPub = CVImgSubPub()
+center_pub = rospy.Publisher('center', Int32, queue_size=1)
+slope_pub = rospy.Publisher('slope', Float64, queue_size=1)
 rospy.spin()
         
