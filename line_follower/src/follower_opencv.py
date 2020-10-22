43890c52928b35e3b3ff479c933d5c39e89a9561
August Soderberg <asoderberg@brandeis.edu>
2020-10-22 18:33:00 +0000

    Add find_middle_white and find_closest_white algorithms

--- line_follower/src/follower_opencv.py
+++ line_follower/src/follower_opencv.py
@@ -2,7 +2,10 @@
 import rospy, cv2, cv_bridge, numpy
 from constants import *
 from sensor_msgs.msg import Image
-from std_msgs.msg import Int32, Float64
+from std_msgs.msg import Int32, Float64, Bool, Int32MultiArray
+
+def dist_between(ax, ay, bx, by):
+    return (((bx - ax)**2) + ((by - ay)**2))**.5
 
 def find_middle_white(image, horiz):
         horiz = int(horiz)
@@ -18,11 +21,27 @@ def find_middle_white(image, horiz):
                     last_white = i 
         return (last_white + first_white) // 2
 
+def find_closest_white(image):
+    target = [IMAGE_HEIGHT - 1, IMAGE_WIDTH // 2]
+    closest_white = [-1, -1]
+    for i in range(0, IMAGE_WIDTH):
+        for j in range(0, IMAGE_HEIGHT):
+            if (image[j, i] == WHITE and dist_between(i, j, target[1], target[0]) < 
+                dist_between(closest_white[1], closest_white[0], target[1], target[0])):
+                closest_white[0] = j
+                closest_white[1] = i
+    return closest_white
+
+
+
 class CVImgSubPub:
     def __init__(self):
         self.bridge = cv_bridge.CvBridge()
         self.image_sub = rospy.Subscriber('camera/rgb/image_raw', Image, self.image_callback)
-        self.image_pub = rospy.Publisher('masked_img', Image, queue_size=1)
+        self.line_found_pub = rospy.Publisher('line_found', Bool, queue_size=1)
+        self.closest_white_pub = rospy.Publisher('closest_white', Int32MultiArray, queue_size=1)
+        self.center_pub = rospy.Publisher('center', Int32, queue_size=1)
+        self.slope_pub = rospy.Publisher('slope', Float64, queue_size=1)
 
     def image_callback(self, msg):
         image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
@@ -32,9 +51,13 @@ class CVImgSubPub:
         image = cv2.inRange(image, lower_yellow, upper_yellow)
         top = find_middle_white(image, TOP_HORIZONTAL)
         bottom = find_middle_white(image, IMAGE_HEIGHT - 1)
-        center_pub.publish((top + bottom) // 2)
-        slope_pub.publish((top - bottom) / (IMAGE_HEIGHT - TOP_HORIZONTAL))
-        self.image_pub.publish(self.bridge.cv2_to_imgmsg(image))
+        if (top == -1 or bottom == -1):
+            self.line_found_pub.publish(False)
+            self.closest_white_pub.publish(find_closest_white(image))
+        else:
+            self.line_found_pub.publish(True)
+            self.center_pub.publish((top + bottom) // 2)
+            self.slope_pub.publish((top - bottom) / (IMAGE_HEIGHT - TOP_HORIZONTAL))
         print((top + bottom) // 2, (top - bottom) / (IMAGE_HEIGHT - TOP_HORIZONTAL))
 
     
@@ -42,7 +65,5 @@ class CVImgSubPub:
 
 rospy.init_node('cv_img_sub_pub')
 cvImgSubPub = CVImgSubPub()
-center_pub = rospy.Publisher('center', Int32, queue_size=1)
-slope_pub = rospy.Publisher('slope', Float64, queue_size=1)
 rospy.spin()
         
